17db0db6a94c5e662fca8af0b918c240f2bf9600
Inaki Vazquez <ivazquez@deusto.es>
2024-05-31 23:34:51 +0200

    Renaming of envs

--- ur5/envs/__init__.py
+++ ur5/envs/__init__.py
@@ -2,7 +2,7 @@ from gymnasium.envs.registration import register
 
 # Register the environment
 register(
-    id='UR5Gym/Box-v0',
+    id='URGym/Box-v0',
     entry_point='ur5.envs.env_box_v0:BoxManipulation',
     max_episode_steps=200,
     kwargs=dict(button_touch_mode='any')
@@ -10,7 +10,7 @@ register(
 
 
 register(
-    id='UR5Gym/CubesPush-v0',
+    id='URGym/CubesPush-v0',
     entry_point='ur5.envs.env_cubes_push_v0:CubesPush',
     max_episode_steps=50,
 )
@@ -18,7 +18,7 @@ register(
 
 # Register the environment
 register(
-    id='UR5Gym/CubesGrasp-v0',
+    id='URGym/CubesGrasp-v0',
     entry_point='ur5.envs.env_cubes_grasp_v0:CubesGrasp',
     max_episode_steps=20,
 )
